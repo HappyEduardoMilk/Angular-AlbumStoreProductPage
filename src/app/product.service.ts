58cc5318e87e1f77ccba16a71c7000966d2a41fa
DIR\eduardo.de.leon <eduardo.de.leon@accenture.com>
2019-01-10 10:43:35 -0600

    Completed the Module

---
+++ src/app/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable(
+  // { providedIn: 'root' }
+)
+
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json';
+
+  getAlbum = (id: number) => {
+    
+    return this._http.get(this._albumUrl)
+      .map(response => { return response.json() });
+
+  }
+
+  constructor(private _http: Http) { }
+
+}
